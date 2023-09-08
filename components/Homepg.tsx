6a103c3507c0b4873d07aac50ea16cc2f060fd2a
ohshinbhat <ohshinb@gmail.com>
2023-09-09 05:04:48 +0530

    layout

---
+++ components/Homepg.tsx
@@ -0,0 +1,35 @@
+import Image from 'next/image'
+import logo from '@/public/logo.svg'
+import Link from 'next/link'
+
+const Homepg = () => {
+  return (
+    <main className="min-h-screen min-w-screen bg-homebg2 bg-opacity-100 bg-cover bg-center">
+        <div>
+            <nav className="flex flex-row justify-between items-center text-white min-w-screen mx-[100px]">
+
+                <div className="flex flex-row items-center gap-2 font-medium mt-[30px]">
+                    <Image src={logo} alt="Rewire logo" className="w-max cursor-pointer" />
+                    <div className=''>Rewire</div>
+                </div>
+                <div className='flex flex-row items-center gap-12 font-medium text-white mt-[30px]'>
+                    <Link href='/about'>About Rewire</Link>
+                    <Link href='/blogs'>Blogs</Link>
+                    <Link href='/contact'>Contact</Link>
+                    <a className='py-[12px] px-[44px] bg-[#000da1] font-bold rounded-[1.6rem] cursor-pointer'>Get the App</a>
+                </div>
+
+            </nav>
+            <div className='flex flex-col items-end mx-[100px] justify-center mt-[30vh] text-white'>
+                <div className="font-bold">Rewire your brain</div>
+                <div className=''>Brain Training Personalized to your needs.</div>
+                <a className="mt-8 px-3 py-9 bg-[#000da1] font-bold w-[209px] rounded-[30px] cursor-pointer">Try for free</a>
+            </div>
+        </div>
+
+
+    </main>
+  )
+}
+
+export default Homepg
---
+++ components/Works.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image"
+import works from "@/public/brainz.jpg"
+const Works = () => {
+  return (
+    <main>
+        <div className="flex flex-row items-center mx-[100px] my-[80px] gap-[70px] justify-between">
+            <div className="flex-[45%] flex flex-col">
+                <div className="font-bold text-[#000da1] mb-5">How Rewire Works</div>
+
+                <p className="font-normal text-[#000da1] flex flex-col">
+                    <span className="mt-[15px]">Rewire&apos;s team of psychologists know that everyone&apos;s different, and not all of us can afford to sit for hours a day playing games. <br /></span>
+                    <span className="mt-[15px]">So we came up with a system that takes <span className='font-bold'>only 20 minutes a day.</span> <br /></span>
+                    <span className="mt-[15px]">Our carefully crafted games target key areas such as memory, attention, problem-solving, and more. By consistently challenging your brain, you'll notice improved focus, enhanced memory retention, and sharper cognitive skills in your everyday life. <br /></span>
+                </p>
+                <a className="mt-8 bg-[#000da1] text-white rounded-[30px] cursor-pointer p-5">Get Started</a>
+
+
+
+            </div>
+            <div className="items-center content-center text-center">
+                <Image src={works} alt="brain" className="scale-[80%] self-center"/>
+
+            </div>
+
+        </div>
+    </main>
+  )
+}
+
+export default Works
